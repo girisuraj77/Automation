731bace31bb1424c967f770ca9e154e643bb8c46
surajgiri <girisuraj9739@gmail.com>
2025-03-23 10:32:05 +0530

    variable

---
+++ src/Javalibarary/Variable_multcomment.java
@@ -0,0 +1,23 @@
+package Javalibarary;
+
+import javax.xml.transform.Source;
+
+public class Variable_multcomment {
+
+    public static void main(String[] args) {
+        /**
+         *multiple comment
+         *author  - suraj giri
+         *
+         *
+         **/
+        int a = 10;
+        System.out.println(a);
+
+
+    }
+
+
+
+
+}
