b8cf1d1a9e5d9314e43e2a04e94f9c2eed5b2e43
surajgiri <girisuraj9739@gmail.com>
2025-03-23 10:12:06 +0530

    variable

---
+++ src/Varia_001.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Varia_001 {
+
+    public static void main(String[] args) {
+
+        int Test_G = 100;
+
+        //int = data type
+        // Test_G = Data type
+        // 100-> values
+
+        System.out.println(Test_G);
+
+
+    }
+}
